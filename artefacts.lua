a05ff960bd276a26b9fc54414dddaf2be943e61f
Eric Rustrum <rustrumek@gmail.com>
2021-08-24 12:56:25 -0700

    Added gold and steel artefacts.

--- artefacts.lua
+++ artefacts.lua
@@ -1,4 +1,9 @@
-minetest.register_craftitem("minetest-test-mod:artefact_batte", {
-	description = S("Ancient Batte Artefact"),
-	tiles = {"batte_artifact.png"},
+minetest.register_craftitem("minetest-test-mod:steel_artefact_batte", {
+	description = S("Ancient Steel Batte Artefact"),
+	tiles = {"batte_artifact_steel.png"},
+})
+
+minetest.register_craftitem("minetest-test-mod:gold_artefact_batte", {
+	description = S("Ancient Gold Batte Artefact"),
+	tiles = {"batte_artifact_gold.png"},
 })
